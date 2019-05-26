67f5926ec05e968b7c57370e8648a8d1725a8255
Emveez <emiliovegaezpeleta@gmail.com>
2019-05-26 11:20:57 +0200

    Update download.sh

--- headDetector/download.sh
+++ headDetector/download.sh
@@ -1,2 +1,2 @@
-wget "https://1drv.ws/u/s!AiKA9MUDJmd8guI9JAYiPYUsopguUQ"
+wget "https://onedrive.live.com/download?cid=7C672603C5F48022&resid=7C672603C5F48022%2145373&authkey=AHN7M7QSxK7VwPI"
 wget https://pjreddie.com/media/files/darknet53.conv.74
