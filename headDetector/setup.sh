79814518cfbb579b90a1673faba8f8dc9dd26d3d
Emveez <emiliovegaezpeleta@gmail.com>
2019-05-26 11:42:57 +0200

    Update setup.sh

--- headDetector/setup.sh
+++ headDetector/setup.sh
@@ -1,4 +1,4 @@
-tar -xvf s!AiKA9MUDJmd8guI9JAYiPYUsopguUQ
+tar -xvf download?cid=7C672603C5F48022&resid=7C672603C5F48022%2145373&authkey=AHN7M7QSxK7VwPI
 mkdir checkpointWeights
 python ./scripts/config.py
 python ./scripts/split.py
